3a88709bba522e149c7f4e5664e056c0cab698bc
Seif Elfayoumy <seifelfayoumy@gmail.com>
2022-11-26 22:12:42 +0200

    init commit

---
+++ FirstProblem/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Collections.Generic;
+
+namespace FirstProblem
+{
+
+  public class CustomKeyValueStore
+  {
+    public string type { get; set; }
+    public string user { get; set; }
+    public string user1 { get; set; }
+    public string user2 { get; set; }
+    public int timestamp { get; set; }
+    public Dictionary<string, string> values { get; set; }
+  }
+
+  public class Broadcast
+  {
+    public string[] broadcast { get; set; }
+    public string user { get; set; }
+    public int timestamp { get; set; }
+    public Dictionary<string, string> values { get; set; }
+  }
+
+  class Program
+  {
+    static Dictionary<string, List<string>> friends = new Dictionary<string, List<string>>();
+
+    static List<int> timestamps = new List<int>();
+    static Dictionary<string, string> updatedValues = new Dictionary<string, string>();
+    static void Main(string[] args)
+    {
+      try
+      {
+        string path = args[0];
+        StreamReader sr = File.OpenText(path);
+        {
+          string s;
+          while ((s = sr.ReadLine()) != null)
+          {
+            CustomKeyValueStore currentEvent = JsonSerializer.Deserialize<CustomKeyValueStore>(s);
+            if (currentEvent.type.Equals("make_friends"))
+            {
+              addFriend(currentEvent.user1, currentEvent.user2);
+              addFriend(currentEvent.user2, currentEvent.user1);
+            }
+            else if (currentEvent.type.Equals("del_friends"))
+            {
+              removeFriend(currentEvent.user1, currentEvent.user2);
+              removeFriend(currentEvent.user2, currentEvent.user1);
+            }
+            else if (currentEvent.type.Equals("update"))
+            {
+              Dictionary<string, string> currentValues = getBroadcastValues(currentEvent.values, currentEvent.timestamp);
+              if (currentValues.Count != 0 && currentEvent.user != null && currentEvent.user != "" && getFriends(currentEvent.user) != null && getFriends(currentEvent.user).Length != 0)
+              {
+                Broadcast newBroadcast = new Broadcast();
+                newBroadcast.broadcast = getFriends(currentEvent.user);
+                newBroadcast.user = currentEvent.user;
+                newBroadcast.timestamp = currentEvent.timestamp;
+                newBroadcast.values = currentValues;
+                Console.WriteLine(JsonSerializer.Serialize(newBroadcast));
+              }
+            }
+          }
+        }
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e);
+      }
+
+    }
+
+    public static string[] getFriends(string user)
+    {
+      if (friends.ContainsKey(user))
+      {
+        return friends[user].ToArray();
+      }
+      return null;
+    }
+    public static void addFriend(string user, string friend)
+    {
+      if (friends.ContainsKey(user))
+      {
+        friends[user].Add(friend);
+      }
+      else
+      {
+        friends.Add(user, new List<string>());
+        friends[user].Add(friend);
+      }
+    }
+
+    public static void removeFriend(string user, string friend)
+    {
+      if (friends.ContainsKey(user))
+      {
+        friends[user].Remove(friend);
+      }
+
+    }
+
+    public static Dictionary<string, string> getBroadcastValues(Dictionary<string, string> values, int timestamp)
+    {
+      Boolean newTimestamp = true;
+      Dictionary<string, string> result = new Dictionary<string, string>();
+      foreach (int stamp in timestamps)
+      {
+        if (timestamp <= stamp)
+        {
+          newTimestamp = false;
+        }
+      }
+      foreach (KeyValuePair<string, string> valuePair in values)
+      {
+        if (newTimestamp)
+        {
+          result.Add(valuePair.Key, valuePair.Value);
+          updatedValues[valuePair.Key] = valuePair.Value;
+        }
+        else
+        {
+          if (!updatedValues.ContainsKey(valuePair.Key))
+          {
+            result.Add(valuePair.Key, valuePair.Value);
+            updatedValues.Add(valuePair.Key, valuePair.Value);
+          }
+          else if (!updatedValues[valuePair.Key].Equals(valuePair.Value))
+          {
+            updatedValues[valuePair.Key] = valuePair.Value;
+            result.Add(valuePair.Key, valuePair.Value);
+          }
+        }
+      }
+
+      timestamps.Add(timestamp);
+      return result;
+    }
+  }
+}
---
+++ SecondProblem/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Collections.Generic;
+using System.Threading;
+using System.Collections;
+
+namespace SecondProblem
+{
+  public class Update
+  {
+    public string type { get; set; }
+    public string user { get; set; }
+    public int timestamp { get; set; }
+    public Dictionary<string, string> values { get; set; }
+  }
+  class Program
+  {
+
+    static Dictionary<string, Dictionary<string, List<KeyValuePair<int, string>>>> userValuesWithTime = new Dictionary<string, Dictionary<string, List<KeyValuePair<int, string>>>>();
+    static Dictionary<string, Dictionary<string, string>> output = new Dictionary<string, Dictionary<string, string>>();
+
+    static List<Thread> threads = new List<Thread>();
+
+    static void Main(string[] args)
+    {
+      try
+      {
+        string path = args[0];
+        StreamReader sr = File.OpenText(path);
+        string s;
+
+        while ((s = sr.ReadLine()) != null)
+        {
+          //   Thread th = new Thread(() => processIncomingUpdate(s));
+          //   threads.Add(th);
+          //   th.Start();
+          processIncomingUpdate(s);
+        }
+
+        // foreach (Thread thread in threads)
+        // {
+        //   thread.Join();
+        // }
+
+        printOutput();
+
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e);
+      }
+    }
+
+    public static void processIncomingUpdate(string json)
+    {
+      Update incomingUpdate = JsonSerializer.Deserialize<Update>(json);
+      KeyValuePair<string, string> incomingValues = new KeyValuePair<string, string>();
+      foreach (KeyValuePair<string, string> kvp in incomingUpdate.values)
+      {
+        incomingValues = kvp;
+      }
+
+      lock (userValuesWithTime)
+      {
+        if (!userValuesWithTime.ContainsKey(incomingUpdate.user))
+        {
+          userValuesWithTime[incomingUpdate.user] = new Dictionary<string, List<KeyValuePair<int, string>>>();
+        }
+        if (!userValuesWithTime[incomingUpdate.user].ContainsKey(incomingValues.Key))
+        {
+          userValuesWithTime[incomingUpdate.user][incomingValues.Key] = new List<KeyValuePair<int, string>>();
+        }
+        userValuesWithTime[incomingUpdate.user][incomingValues.Key].Add(new KeyValuePair<int, string>(incomingUpdate.timestamp, incomingValues.Value));
+      }
+    }
+
+    public static void printOutput()
+    {
+      foreach (KeyValuePair<string, Dictionary<string, List<KeyValuePair<int, string>>>> kvp in userValuesWithTime)
+      {
+        output[kvp.Key] = new Dictionary<string, string>();
+        foreach (KeyValuePair<string, List<KeyValuePair<int, string>>> kvp2 in kvp.Value)
+        {
+          int maxTimeStamp = 0;
+          foreach (KeyValuePair<int, string> kvp3 in kvp2.Value)
+          {
+            if (kvp3.Key > maxTimeStamp)
+            {
+              maxTimeStamp = kvp3.Key;
+            }
+          }
+
+          foreach (KeyValuePair<int, string> kvp3 in kvp2.Value)
+          {
+            if (kvp3.Key == maxTimeStamp)
+            {
+              output[kvp.Key][kvp2.Key] = kvp3.Value;
+            }
+          }
+        }
+      }
+
+      Console.WriteLine(JsonSerializer.Serialize(output));
+    }
+  }
+}
